6135bd15d08221ac5c1dca1671deaf43ff616338
Jared Eddy <jaredgeddy@gmail.com>
2018-11-20 09:37:36 -0800

    finished infinite scroll, planning and design page, event gallery page, and started collections

---
+++ wp-content/uploads/alm_templates/default.php
@@ -0,0 +1,5 @@
+<div class="alm-gallery-img">
+   <a href="<?php echo $image['url']; ?>" class="popup">
+      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+   </a>
+</div>
